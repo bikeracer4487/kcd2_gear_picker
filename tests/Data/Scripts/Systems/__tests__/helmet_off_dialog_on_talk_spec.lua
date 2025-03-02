77440db2c50c0963f1739081dfc7f522d1e53e6f
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-02 22:38:43 +0000

    chore: Catch errors & log.

--- tests/Data/Scripts/Systems/__tests__/helmet_off_dialog_on_talk_spec.lua
+++ tests/Data/Scripts/Systems/__tests__/helmet_off_dialog_on_talk_spec.lua
@@ -37,18 +37,29 @@ function makeFactory()
     end }
 
     dofile("src/Data/Scripts/helmet_off_dialog/helmet_off_dialog.lua")
+    dofile("src/Data/Scripts/helmet_off_dialog/utils/error.lua")
     dofile("src/Data/Scripts/helmet_off_dialog/on_talk_event.lua")
     dofile("src/Data/Scripts/helmet_off_dialog/config.lua")
     dofile("src/Data/Scripts/helmet_off_dialog/utils/log.lua")
     dofile("src/Data/Scripts/helmet_off_dialog/utils/timed_trigger.lua")
     dofile("src/Data/Scripts/helmet_off_dialog/equipped_item.lua")
     dofile("src/Data/Scripts/helmet_off_dialog/equipment.lua")
+
+    local Error = mock(HelmetOffDialog.ClassRegistry.Error)
+    local Log = mock(HelmetOffDialog.ClassRegistry.Log, true)
     local OnTalkEvent = mock(HelmetOffDialog.ClassRegistry.OnTalkEvent, true)
     local HelmetOffDialog = mock(HelmetOffDialog, true)
+    HelmetOffDialog.log = function()
+        return Log
+    end
+    HelmetOffDialog.error = function()
+        return Error
+    end
     HelmetOffDialog.onTalkEvent = function()
         return OnTalkEvent
     end
     spy.on(HelmetOffDialog, "onTalkEvent")
+    spy.on(HelmetOffDialog, "error")
     --HelmetOffDialog.onTalkEvent = stub(OnTalkEvent)
 
     _G.BasicAIActions = {
