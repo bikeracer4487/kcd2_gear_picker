2aaac29e2f6a6d6169a093cee9af530a08808670
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-02 22:30:25 +0000

    feat: Trigger the on talk event: create event.

---
+++ src/Data/Scripts/Systems/helmet_off_dialog_on_talk.lua
@@ -0,0 +1,17 @@
+--local original_BasicAIActions_onTalk = BasicAIActions.OnTalk;
+
+function BasicAIActions:OnTalk(...)
+    --local log = HelmetOffDialog:log()
+    --log:info("BasicAIActions:OnTalk")
+    --local error = HelmetOffDialog:error()
+
+    --error:catch(function()
+    local event = HelmetOffDialog:onTalkEvent()
+    --event:handle()
+    --end)
+    --
+    -- TODO: It is unclear actually if the following is required.
+    --  Need to test if having another mod that uses this action will still be
+    -- working
+    --return original_BasicAIActions_onTalk(self, ...)
+end
---
+++ tests/Data/Scripts/Systems/__tests__/helmet_off_dialog_on_talk_spec.lua
@@ -0,0 +1,61 @@
+dofile("tests/mock_helmet_off_dialog_spec.lua")
+
+describe("triggers the on talk event", function()
+    it("creates the event", function()
+        local factory = makeFactory()
+        factory.BasicAIActions:OnTalk()
+        assert.spy(factory.HelmetOffDialog.onTalkEvent).was.called(1)
+    end)
+    --it("triggers the event", function()
+    --    local factory = makeFactory()
+    --    factory.BasicAIActions:OnTalk()
+    --    assert.spy(factory.HelmetOffDialog.onTalkEvent().handle).was.called(1)
+    --end)
+end)
+
+function makeFactory()
+    --mockHelmetOffDialog()
+
+    _G.Log = { info = function()
+    end, error = function()
+    end }
+    _G.Error = { catch = function(callback)
+        callback()
+    end }
+
+    _G.Script = {
+        SetTimer = function()
+        end,
+        LoadScript = function()
+        end
+    }
+    _G.System = { LogAlways = function()
+    end }
+    _G.Config = { LogAlways = function()
+    end }
+    _G.player = { Lorem = function()
+    end }
+
+    dofile("src/Data/Scripts/helmet_off_dialog/helmet_off_dialog.lua")
+    dofile("src/Data/Scripts/helmet_off_dialog/on_talk_event.lua")
+    dofile("src/Data/Scripts/helmet_off_dialog/config.lua")
+    dofile("src/Data/Scripts/helmet_off_dialog/utils/log.lua")
+    dofile("src/Data/Scripts/helmet_off_dialog/utils/timed_trigger.lua")
+    dofile("src/Data/Scripts/helmet_off_dialog/equipped_item.lua")
+    dofile("src/Data/Scripts/helmet_off_dialog/equipment.lua")
+    local OnTalkEvent = mock(HelmetOffDialog.ClassRegistry.OnTalkEvent, true)
+    local HelmetOffDialog = mock(HelmetOffDialog, true)
+    HelmetOffDialog.onTalkEvent = function()
+        return OnTalkEvent
+    end
+    spy.on(HelmetOffDialog, "onTalkEvent")
+    --HelmetOffDialog.onTalkEvent = stub(OnTalkEvent)
+
+    _G.BasicAIActions = {
+        OnTalk = function()
+        end
+    }
+    dofile("src/Data/Scripts/Systems/helmet_off_dialog_on_talk.lua")
+
+    return { BasicAIActions = BasicAIActions, HelmetOffDialog = HelmetOffDialog }
+end
