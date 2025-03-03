b66e8c24ec2dbf946f00953842d14dd844542683
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-03 23:46:33 +0000

    feat: Easily run a callback after a time.

---
+++ src/Data/Scripts/helmet_off_dialog/utils/timed_trigger.lua
@@ -0,0 +1,36 @@
+--- @class TimedTrigger
+--- @field new fun(self: TimedTrigger, script: _G.Script.SetTimer): TimedTrigger
+--- @field start fun(self: TimedTrigger, intervalMs: number, checkCallback: function, runCallback: function)
+--- @field log Log
+--- @field script _G.Script
+local TimedTrigger = {
+    log = nil,
+    setTimer = nil,
+    new = function(self, log, script)
+        if HelmetOffDialog.__factories.timedTrigger then
+            return HelmetOffDialog.__factories.timedTrigger
+        end
+        local instance = { log = log, script = script, }
+        setmetatable(instance, { __index = self })
+        HelmetOffDialog.__factories.timedTrigger = instance
+
+        return instance
+    end,
+
+    start = function(self, intervalMs, checkCallback, runCallback)
+        --- @type TimedTrigger
+        local this = self
+
+        self.log:info("TimedTrigger.start")
+
+        this.script.SetTimer(intervalMs, function()
+            if checkCallback() then
+                runCallback()
+            else
+                this:start(intervalMs, checkCallback, runCallback)
+            end
+        end)
+    end,
+}
+
+HelmetOffDialog.ClassRegistry.TimedTrigger = TimedTrigger
