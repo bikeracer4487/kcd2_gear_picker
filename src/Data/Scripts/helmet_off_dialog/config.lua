434b36af191a5e5c9b549d15a00e30e52c6a4f6b
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-03 20:59:53 +0000

    feat: Detect development vs production environments.

---
+++ src/Data/Scripts/helmet_off_dialog/config.lua
@@ -0,0 +1,25 @@
+--- @class Config
+--- @field isProduction fun(self: Config): boolean
+--- @field environment string
+--- @field new Config
+
+local Config = {
+    environment = nil,
+    new = function(self, environment)
+        if HelmetOffDialog.__factories.config then
+            return HelmetOffDialog.__factories.config
+        end
+        local instance = { environment = environment, }
+        setmetatable(instance, { __index = self })
+        HelmetOffDialog.__factories.config = instance
+
+        return instance
+    end,
+    isProduction = function(self)
+        --- @type Config
+        local config = self
+        return config.environment == "prod"
+    end
+}
+
+HelmetOffDialog.ClassRegistry.Config = Config
