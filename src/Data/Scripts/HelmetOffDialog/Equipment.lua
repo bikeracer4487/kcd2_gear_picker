9e9e5af3e8154750d2bee00af9bce61f515a70cc
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-06 07:27:50 +0000

    refactor: Equipment is now a controller for putting on / taking off equipment.

--- src/Data/Scripts/HelmetOffDialog/Equipment.lua
+++ src/Data/Scripts/HelmetOffDialog/Equipment.lua
@@ -1,162 +1,88 @@
 --- @class Equipment
---- @field new fun(self: Equipment, log: Log, player: _G.player, equippedItem: EquippedItem): Equipment
---- @field equippedItem EquippedItem
+--- @field new fun(self: Equipment, helmetOffDialog: HelmetOffDialog, log: Log, player: _G.player, equippedItem: EquippedItem, itemCategory: ItemCategory, unequipGear: UnequipGear): Equipment
+--- @field putOnCoif fun(self: Equipment)
+--- @field putOnHelmet fun(self: Equipment)
+--- @field putOnHeadChainmail fun(self: Equipment)
+--- @field log Log
+--- @field unequipGear UnequipGear
 local Equipment = {
-    new = function(self, log, player, equippedItem)
-        log:info("[Equipment] Creating new Equipment instance")
-        if HelmetOffDialog.__factories and HelmetOffDialog.__factories.equipment then
-            log:info("[Equipment] Returning cached instance")
-            return HelmetOffDialog.__factories.equipment
+    new = function(self, helmetOffDialog, log, player, unequipGear)
+        log:info("Equipment.new")
+        if helmetOffDialog.__factories.equipment then
+            return helmetOffDialog.__factories.equipment
         end
         local instance = {
             log = log,
             player = player,
-            equippedItem = equippedItem,
-            storedItems = {}
+            unequipGear = unequipGear,
+            helmetOffDialog = helmetOffDialog,
+            unequippedHelmet = nil,
+            unequippedHeadChainmail = nil,
+            unequippedCoif = nil
         }
         setmetatable(instance, { __index = self })
-        HelmetOffDialog.__factories = HelmetOffDialog.__factories or {}
-        HelmetOffDialog.__factories.equipment = instance
-        log:info("[Equipment] New instance created")
+        helmetOffDialog.__factories.equipment = instance
+        log:info("Equipment New instance created")
         return instance
     end,
 
-    equip = function(self, itemType)
-        self.log:info("[Equipment] Trying to equip " .. itemType)
-        local inventoryItem = self.storedItems[itemType]
-        if not inventoryItem then
-            self.log:info("[Equipment] No " .. itemType .. " in storage")
-            return
+    putOnHelmet = function(self)
+        self.log:info("Equipment.putOnHelmet")
+        if self.unequippedHelmet then
+            self.player.actor:EquipInventoryItem(self.unequippedHelmet)
+            self.unequippedHelmet = nil
         end
-
-        local item = ItemManager.GetItem(inventoryItem)
-        if not item then
-            self.log:info("[Equipment] Invalid " .. itemType .. " in storage")
-            return
-        end
-
-        local isTypeCheck = self["is" .. itemType]
-        if not isTypeCheck(self, inventoryItem) then
-            self.log:info("[Equipment] Stored item is not a " .. itemType)
-            return
-        end
-
-        local itemName = ItemManager.GetItemName(item.class)
-        self.player.actor:EquipInventoryItem(inventoryItem)
-        self.storedItems[itemType] = nil
-        self.log:info("[Equipment] Equipped " .. itemType .. ": " .. itemName)
     end,
 
-    unequipIfEquipped = function(self, inventoryItem, itemType, callback)
+    putOnHeadChainmail = function(self)
+        self.log:info("Equipment.putOnHeadChainmail")
+        if self.unequippedHeadChainmail then
+            self.player.actor:EquipInventoryItem(self.unequippedHeadChainmail)
+            self.unequippedHeadChainmail = nil
+        end
+    end,
+
+    putOnCoif = function(self)
+        self.log:info("Equipment.putOnCoif")
+        if self.unequippedCoif then
+            self.player.actor:EquipInventoryItem(self.unequippedCoif)
+            self.unequippedCoif = nil
+        end
+    end,
+
+    takeOffHelmet = function(self, callback)
         --- @type Equipment
-        local this = sef
-        local item = ItemManager.GetItem(inventoryItem)
-        if not item then
-            self.log:info("[Equipment] Invalid item")
-            callback()
-            return
-        end
-
-        local itemName = ItemManager.GetItemName(item.class)
-        local isTypeCheck = self["is" .. itemType]
-        if not isTypeCheck(self, inventoryItem) then
-            self.log:info("[Equipment] Not a " .. itemType .. ": " .. itemName)
-            callback()
-            return
-        end
-
-        this.equippedItem:isEquipped(inventoryItem, function(isEquipped)
-            if not isEquipped then
-                self.log:info("[Equipment] " .. itemType .. " not equipped: " .. itemName)
-                callback()
-                return
+        local this = self
+        this.log:info("Equipment.takeOffHelmet")
+        this.unequipGear:takeOff("Helmet", function(unequippedHelmet)
+            if unequippedHelmet then
+                self.unequippedHelmet = unequippedHelmet
             end
-
-            self.player.actor:UnequipInventoryItem(inventoryItem)
-            self.storedItems[itemType] = inventoryItem
-            self.log:info("[Equipment] Unequipped " .. itemType .. ": " .. itemName)
             callback()
         end)
     end,
 
-    unequip = function(self, itemType)
-        self.log:info("[Equipment] Starting to unequip " .. itemType)
-        local inventory = self.player.inventory:GetInventoryTable()
-
-        for _, inventoryItem in pairs(inventory) do
-            local item = ItemManager.GetItem(inventoryItem)
-            if item then
-                local isTypeCheck = self["is" .. itemType]
-                if isTypeCheck(self, inventoryItem) then
-                    self:unequipIfEquipped(inventoryItem, itemType, function()
-                        self.log:info("[Equipment] Finished unequipping " .. itemType)
-                    end)
-                    return
-                end
+    takeOffHeadChainmail = function(self, callback)
+        self.log:info("Equipment.takeOffHeadChainmail")
+        self.unequipGear:takeOff("HeadChainmail", function(unequippedHeadChainmail)
+            if unequippedHeadChainmail then
+                self.unequippedHeadChainmail = unequippedHeadChainmail
             end
-        end
-
-        self.log:info("[Equipment] No equipped " .. itemType .. " found")
+            callback()
+        end)
     end,
 
-    isHelmet = function(self, inventoryItem)
-        local item = ItemManager.GetItem(inventoryItem)
-        local itemName = ItemManager.GetItemName(item.class)
-        self.log:info("[Equipment] isHelmet: itemName: " .. itemName)
-        local itemUIName = ItemManager.GetItemUIName(item.class)
-        self.log:info("[Equipment] isHelmet: itemUIName: " .. itemUIName)
-        return string.lower(itemName):find("kettle")
-                or string.lower(itemName):find("bascinet")
-                -- KCD 1
-                or string.lower(itemName):find("helmet")
-    end,
-
-    -- Source: https://github.com/OVNI-modding/kcd_inventory_sorting_generator/blob/eab04397c7cf877048a2ac3d250f72793d313e1a/categorizedItems.ini#L217C10-L217C20
-    isHeadChainmail = function(self, inventoryItem)
-        local item = ItemManager.GetItem(inventoryItem)
-        local itemName = ItemManager.GetItemName(item.class)
-        self.log:info("[Equipment] isHeadChainmail: itemName: " .. itemName)
-        local itemUIName = ItemManager.GetItemUIName(item.class)
-        self.log:info("[Equipment] isHeadChainmail: itemUIName: " .. itemUIName)
-        return string.lower(itemName):find("coifmail")
-                -- KCD 1
-                or string.lower(itemUIName):find("nm_ca_collar")
-                or string.lower(itemUIName):find("nm_ca_hood")
-    end,
-
-    isCoif = function(self, inventoryItem)
-        local item = ItemManager.GetItem(inventoryItem)
-        local itemName = ItemManager.GetItemName(item.class)
-        self.log:info("[Equipment] isCoif: itemName: " .. itemName)
-        local itemUIName = ItemManager.GetItemUIName(item.class)
-        self.log:info("[Equipment] isCoif: itemUIName: " .. itemUIName)
-        return string.lower(itemName):find("coif")
-                or string.lower(itemName):find("g_hood_")
-    end,
-
-    equipHelmet = function(self)
-        self:equip("Helmet")
-    end,
-    equipHeadChainmail = function(self)
-        self:equip("HeadChainmail")
-    end,
-    equipCoif = function(self)
-        self:equip("Coif")
-    end,
-    takeOffHelmet = function(self, callback)
-        self.log:info("[Equipment] unequipHelmet")
-        self:unequip("Helmet")
-    end,
-    unequipHeadChainmail = function(self)
-        self.log:info("[Equipment] unequipHeadChainmail")
-        self:unequip("HeadChainmail")
-    end,
-    unequipCoif = function(self)
-        self.log:info("[Equipment] unequipCoif")
-        self:unequip("Coif")
+    takeOffCoif = function(self, callback)
+        self.log:info("Equipment.takeOffCoif")
+        self.unequipGear:takeOff("Coif", function(unequippedCoif)
+            if unequippedCoif  then
+                self.unequippedCoif = unequippedCoif
+            end
+            callback()
+        end)
     end
 }
 
-HelmetOffDialog.ClassRegistry.Equipment = Equipment
+_G.HelmetOffDialog.ClassRegistry.Equipment = Equipment
 
 return Equipment
