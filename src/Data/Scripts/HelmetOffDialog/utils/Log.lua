ee73c2f8af0c58bcf1fc5f677a663b201211f3c8
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-08 17:15:02 +0000

    chore: Improve log to accept any number of arguments and print tables.

--- src/Data/Scripts/HelmetOffDialog/utils/Log.lua
+++ src/Data/Scripts/HelmetOffDialog/utils/Log.lua
@@ -23,16 +23,51 @@ local Log = {
                 localTime.year + 1900, localTime.mon + 1, localTime.mday,
                 localTime.hour, localTime.min, localTime.sec)
     end,
-    info = function(self, message)
+    _log = function(self, level, colorCode, ...)
+        local localTime = self:getLocalTime()
+        local args = {...}
+        local messageParts = {}
+
+        for _, arg in ipairs(args) do
+            if type(arg) == "table" then
+                local isSequential = true
+                for k in pairs(arg) do
+                    if type(k) ~= "number" or k < 1 or math.floor(k) ~= k then
+                        isSequential = false
+                        break
+                    end
+                end
+
+                if isSequential then
+                    local tableParts = {}
+                    for _, v in ipairs(arg) do
+                        tableParts[#tableParts + 1] = tostring(v)
+                    end
+                    messageParts[#messageParts + 1] = table.concat(tableParts, ", ")
+                else
+                    local tableParts = {}
+                    for k, v in pairs(arg) do
+                        tableParts[#tableParts + 1] = tostring(k) .. "=" .. tostring(v)
+                    end
+                    messageParts[#messageParts + 1] = "{" .. table.concat(tableParts, ", ") .. "}"
+                end
+            else
+                messageParts[#messageParts + 1] = tostring(arg)
+            end
+        end
+
+        local message = table.concat(messageParts, " ")
+        System.LogAlways(string.format("%s[%s] [%s.%s] %s",
+                colorCode, localTime, self.modName, level, message))
+    end,
+    info = function(self, ...)
         local config = self.config
         if not config:isProduction() then
-            local localTime = self:getLocalTime()
-            System.LogAlways("$5[" .. localTime .. "] [" .. self.modName .. ".INFO] " .. message)
+            self:_log("INFO", "$5", ...)
         end
     end,
-    error = function(self, message)
-        local localTime = self:getLocalTime()
-        System.LogAlways("$4[" .. localTime .. "] [" .. self.modName .. ".ERROR] " .. message)
+    error = function(self, ...)
+        self:_log("ERROR", "$4", ...)
     end
 }
 
