79f98dde37e4eca88d7554ddc9a789ef87574add
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-10 03:41:54 +0000

    refactor: Deduplicate test factories.

--- tests/Data/Scripts/HelmetOffDialog/OnTalkEvent_MainVersion_spec.lua
+++ tests/Data/Scripts/HelmetOffDialog/OnTalkEvent_MainVersion_spec.lua
@@ -38,38 +38,7 @@ describe("coif", function()
 end)
 
 function makeFactory(args)
-    dofile("src/Data/Scripts/HelmetOffDialog/utils/dd.lua")
-    local mockHelmetOffDialog = dofile("tests/HodMock.lua")
-    local helmetOffDialogFactory = mockHelmetOffDialog(mock, spy)
-    local helmetOffDialog = helmetOffDialogFactory.HelmetOffDialog
-    local OnTalkEvent = dofile("src/Data/Scripts/HelmetOffDialog/OnTalkEvent.lua")
-    local Equipment = dofile("src/Data/Scripts/HelmetOffDialog/Equipment.lua")
-    local equipment = mock(Equipment, true)
-    equipment.takeOffHelmet = spy.new(function(self, callback) callback() end)
-    equipment.takeOffHeadChainmail = spy.new(function(self, callback) callback() end)
-    equipment.takeOffCoif = spy.new(function(self, callback) callback() end)
-    local TalkEndedEvent = dofile("src/Data/Scripts/HelmetOffDialog/TalkEndedEvent.lua")
-    local talkEndedEvent = mock(TalkEndedEvent, true)
-    local log = { info = spy.new(function() end) }
+    local createFactory = dofile("tests/Data/Scripts/HelmetOffDialog/OnTalkEvent_Factory.lua")
 
-    -- Handle named arguments
-    args = args or {}
-    if args.randomVersion then
-        helmetOffDialog.VERSION = "random"
-        if args.mockMathRandomToTruthy ~= nil then
-            math.random = function() return args.mockMathRandomToTruthy and 1 or 0 end
-        end
-    else
-        helmetOffDialog.VERSION = "default"
-    end
-
-    local onTalkEvent = OnTalkEvent:new(helmetOffDialog, log, equipment, talkEndedEvent)
-    spy.on(onTalkEvent, "takeOffHeadChainmail")
-    spy.on(onTalkEvent, "coif")
-    spy.on(onTalkEvent, "takeOffCoif")
-    return {
-        equipment = equipment,
-        onTalkEvent = onTalkEvent,
-        talkEndedEvent = talkEndedEvent
-    }
+    return createFactory(mock, spy, args)
 end
