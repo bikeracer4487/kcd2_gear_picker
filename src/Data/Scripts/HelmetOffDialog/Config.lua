bded9b4093d518bbfc63ee7b10d83fa1dfb22c85
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-16 04:07:34 +0000

    chore: Hide ranged weapon is toggleable.

--- src/Data/Scripts/HelmetOffDialog/Config.lua
+++ src/Data/Scripts/HelmetOffDialog/Config.lua
@@ -2,16 +2,21 @@
 --- @field isProduction fun(self: Config): boolean
 --- @field environment string
 --- @field new Config
+--- @field isHideRangedWeapons fun(self: Config): boolean
+--- @field setHideRangedWeapons fun(self: Config): boolean
 
 local Config = {
     environment = nil,
     new = function(self, environment)
-        if HelmetOffDialog.__factories.config then
-            return HelmetOffDialog.__factories.config
+        if _G.HelmetOffDialog.__factories.config then
+            return _G.HelmetOffDialog.__factories.config
         end
         local instance = { environment = environment, }
-        setmetatable(instance, { __index = self })
-        HelmetOffDialog.__factories.config = instance
+        setmetatable(instance, {
+            __index = self,
+            hide_ranged_weapons = false
+        })
+        _G.HelmetOffDialog.__factories.config = instance
 
         return instance
     end,
@@ -19,9 +24,15 @@ local Config = {
         --- @type Config
         local config = self
         return config.environment == "prod"
+    end,
+    isHideRangedWeapons = function(self)
+        return self.hide_ranged_weapons
+    end,
+    setHideRangedWeapons = function(self, value)
+        self.hide_ranged_weapons = value
     end
 }
 
-HelmetOffDialog.ClassRegistry.Config = Config
+_G.HelmetOffDialog.ClassRegistry.Config = Config
 
 return Config
