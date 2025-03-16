a2b1271dea5d059aadc969116f97c0b1c1dfb73a
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-16 05:42:42 +0000

    chore: Cleanup.

--- src/Data/Scripts/Systems/HelmetOffDialog_Configuration.lua
+++ src/Data/Scripts/Systems/HelmetOffDialog_Configuration.lua
@@ -10,21 +10,3 @@ _G.UIAction.RegisterEventSystemListener(
         "OnLoadingComplete",
         "OnLoadingComplete"
 )
-
-function _G.HelmetOffDialog:setHideRangedWeapons(rawInput)
-    log:info("setHideRangedWeapons called with argument: ", rawInput)
-
-    local sanitized = (tostring(rawInput):match("^%s*=?%s*(.-)%s*$") or "")
-    local setting = sanitized:lower() == "true" or sanitized == "1"
-
-    log:info("Setting hide ranged weapon with value: ", setting)
-
-    local config = _G.HelmetOffDialog:config()
-    config:setHideRangedWeapons(setting)
-end
-
-_G.System.AddCCommand(
-        "helmet_off_dialog__set_hide_ranged_weapons",
-        "HelmetOffDialog:setHideRangedWeapons(%line)",
-        ""
-);
