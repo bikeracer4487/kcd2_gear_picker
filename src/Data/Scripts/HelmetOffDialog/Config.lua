75a2f6f7b8bcaa7e0504b097bc2d12d990ace0b1
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-16 12:32:04 +0000

    fix: Set metatable.

--- src/Data/Scripts/HelmetOffDialog/Config.lua
+++ src/Data/Scripts/HelmetOffDialog/Config.lua
@@ -3,11 +3,11 @@
 --- @field environment string
 --- @field new Config
 --- @field isRanged fun(self: Config): boolean
---- @field setRanged fun(self: Config): boolean
---- @field setRandom fun(self: Config): boolean
+--- @field setRanged fun(self: Config, value: boolean)
 --- @field isRandom fun(self: Config): boolean
---- @field setHelmetOnly fun(self: Config): boolean
+--- @field setRandom fun(self: Config, value: boolean)
 --- @field isHelmetOnly fun(self: Config): boolean
+--- @field setHelmetOnly fun(self: Config, value: boolean)
 
 local Config = {
     environment = nil,
@@ -15,13 +15,14 @@ local Config = {
         if _G.HelmetOffDialog.__factories.config then
             return _G.HelmetOffDialog.__factories.config
         end
-        local instance = { environment = environment, }
-        setmetatable(instance, {
-            __index = self,
-            ranged_variant = false,
-            helmet_only_variant = false,
-            random_variant = false
-        })
+        local instance = {
+            environment = environment,
+            ranged = false,
+            helmet_only = false,
+            random = false
+        }
+
+        setmetatable(instance, { __index = self })
         _G.HelmetOffDialog.__factories.config = instance
 
         return instance
@@ -32,22 +33,22 @@ local Config = {
         return config.environment == "prod"
     end,
     isRanged = function(self)
-        return self.ranged_variant
+        return self.ranged
     end,
     setRanged = function(self, value)
-        self.ranged_variant = self:sanitize(value)
+        self.ranged = self:sanitize(value)
     end,
     isRandom = function(self)
-        return self.random_variant
+        return self.random
     end,
     setRandom = function(self, value)
-        self.random_variant = self:sanitize(value)
+        self.random = self:sanitize(value)
     end,
     isHelmetOnly = function(self)
-        return self.helmet_only_variant
+        return self.helmet_only
     end,
     setHelmetOnly = function(self, value)
-        self.helmet_only_variant = self:sanitize(value)
+        self.helmet_only = self:sanitize(value)
     end,
     sanitize = function(self, value)
         local sanitized = (tostring(value):match("^%s*=?%s*(.-)%s*$") or "")
