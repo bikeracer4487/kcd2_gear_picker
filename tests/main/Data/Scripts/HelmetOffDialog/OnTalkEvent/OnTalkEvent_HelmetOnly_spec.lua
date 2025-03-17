24e5a7af5148c673cf0fca45dbb526eadeb38ab8
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-17 09:17:52 +0000

    test: Assert ranged feature is enabled with the helmet only feature.

--- tests/main/Data/Scripts/HelmetOffDialog/OnTalkEvent/OnTalkEvent_HelmetOnly_spec.lua
+++ tests/main/Data/Scripts/HelmetOffDialog/OnTalkEvent/OnTalkEvent_HelmetOnly_spec.lua
@@ -1,25 +1,48 @@
 it("takes off helmet", function()
-    local factory = makeFactory({ target = "helmet_only" })
+    local factory = makeFactory({ features = "helmet_only" })
     factory.onTalkEvent:handle()
     assert.spy(factory.equipment.takeOffHelmet).was_called(1)
 end)
 
 it("does not queue take off for head chainmail", function()
-    local factory = makeFactory({ target = "helmet_only" })
+    local factory = makeFactory({ features = "helmet_only" })
     factory.onTalkEvent:handle()
     assert.spy(factory.onTalkEvent.takeOffHeadChainmail).was_not_called()
 end)
 
 it("does not queue take off for coifs", function()
-    local factory = makeFactory({ target = "helmet_only" })
+    local factory = makeFactory({ features = "helmet_only" })
     factory.onTalkEvent:handle()
     assert.spy(factory.onTalkEvent.takeOffCoif).was_not_called()
 end)
 
 it("triggers talk ended event", function()
-    local factory = makeFactory({ target = "helmet_only" })
+    local factory = makeFactory({ features = "helmet_only" })
     factory.onTalkEvent:handle()
-    assert.spy(factory.talkEndedEvent.listen).was_called(1)
+    assert.spy(factory.onTalkEvent.handleTalkEndedEvent)
+          .was_called_with(
+            match.is_ref(factory.onTalkEvent),
+            "triggeredByHandler"
+    )
+end)
+
+describe("ranged feature", function()
+    local features = { "helmet_only", "ranged" }
+    it("triggers take off first ranged weapon", function()
+        local factory = makeFactory({ features = features })
+        factory.onTalkEvent:handle()
+        assert.spy(factory.onTalkEvent.takeOffFirstRangedWeapon).was_called(1)
+    end)
+
+    it("triggers talk ended event", function()
+        local factory = makeFactory({ features = features })
+        factory.onTalkEvent:handle()
+        assert.spy(factory.onTalkEvent.handleTalkEndedEvent)
+              .was_called_with(
+                match.is_ref(factory.onTalkEvent),
+                "triggeredByRanged"
+        )
+    end)
 end)
 
 function makeFactory(args)
