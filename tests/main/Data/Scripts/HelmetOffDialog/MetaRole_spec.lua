04c151a73beb551a3f5be8fbd83a1056729b05dd
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-27 21:57:15 +0000

    test: Entity with archery competition linked entities is identifiable.

--- tests/main/Data/Scripts/HelmetOffDialog/MetaRole_spec.lua
+++ tests/main/Data/Scripts/HelmetOffDialog/MetaRole_spec.lua
@@ -9,6 +9,17 @@ describe("hasBathhouseBooking", function()
     end)
 end)
 
+describe("hasArcheryCompetition", function()
+    it("truthy having a valid linked entity", function()
+        local factory = makeFactory({ linkedEntity = { "ShootingRange" } })
+        assert.is_true(factory.metaRole:hasArcheryCompetition(factory.entityName))
+    end)
+    it("falsy having no valid linked entity", function()
+        local factory = makeFactory({ linkedEntity = { "invalid" } })
+        assert.is_false(factory.metaRole:hasArcheryCompetition(factory.entityName))
+    end)
+end)
+
 function makeFactory(args)
     dofile("src/main/Data/Scripts/HelmetOffDialog/utils/dd.lua")
 
--- tests/main/SystemMock.lua
+++ tests/main/SystemMock.lua
@@ -1,10 +1,26 @@
 local function mockSystem(mock, spy, args)
+    local linkedEntity = {
+        GetName = function(self)
+            return args and args.linkedEntity and "LoremShootingRangeIpsum" or nil
+        end
+    }
     local soul = {
         GetMetaRoles = function(self)
             return args and args.metaRoles or "lorem-ipsum"
+        end,
+    }
+    local entity = {
+        soul = soul,
+        CountLinks = function(self)
+            return args and args.linkedEntity and #args.linkedEntity or 0
+        end,
+        GetLink = function(self, index)
+            return args and args.linkedEntity
+                    and args.linkedEntity[1] == "ShootingRange"
+                    and linkedEntity or nil
         end
     }
-    local entity = { soul = soul }
+
     local System = {
         GetEntityByName = function()
             return entity
